533aab85f3552504f88a531dd5f4b5186c384aad
Beamfield <beamfield@gmail.com>
2014-11-21 13:44:18 +0100

    Updated BuildCraft API

--- src/main/java/com/beamfield/tc2/utils/ModHelperBase.java
+++ src/main/java/com/beamfield/tc2/utils/ModHelperBase.java
@@ -5,16 +5,10 @@ import cpw.mods.fml.common.ModAPIManager;
 
 public class ModHelperBase {
 	public static boolean useCofh;
-	public static boolean useBuildcraftTools;
-	public static boolean useBuildcraftTransport;
 	
 	public void register() {}
 	
 	public static void detectMods() {
-
-		useCofh = Loader.isModLoaded("CoFHCore");
-		useBuildcraftTools = ModAPIManager.INSTANCE.hasAPI("BuildCraftAPI|tools");
-		useBuildcraftTransport = ModAPIManager.INSTANCE.hasAPI("BuildCraftAPI|transport");
-		
+		useCofh = Loader.isModLoaded("CoFHCore"); 
 	}
 }
